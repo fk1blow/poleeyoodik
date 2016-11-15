949a42d986c6bf0c227fe3b132deb62289256a34
Dragos Tudorache <dtudorache@YSRO-PORT014s-MacBook-Pro.local>
2016-11-15 19:57:43 +0200

    add strategy and strategy modifier

--- src/App.vue
+++ src/App.vue
@@ -10,31 +10,37 @@ import Rx from 'rxjs';
 import Hello from './components/Hello'
 import ReactivePoller from '../lib/reactive-poller/';
 
-const resource = {url: 'https:/jsonplaceholder.typicode.com/posts/1'};
-const strategy = {timeout: 1000, interval: 5000};
+const strategyModifier = (result, strategy) => {
+  return {...strategy, interval: result.interval}
+}
+
+// const resource = {url: 'https:/jsonplaceholder.typicode.com/posts/1'};
+const resource = {url: 'http://localhost:3000/api/event-info'};
+const strategy = {timeout: 1000, interval: 1000, modifier: strategyModifier};
 
 let x = ReactivePoller.run(resource, strategy)
 
 setTimeout(() => {
   console.log('new sub x')
   x.subscribe(
-    r => console.log("x r:", r),
+    // r => console.log("x r:"),
+    r => {},
     e => console.log("x e:", e))
 }, 500)
 
-setTimeout(() => {
-  console.log('new sub y')
-  x.subscribe(
-    r => console.log("y r:", r),
-    e => console.log("y e:", e))
-}, 6000)
+// setTimeout(() => {
+//   console.log('new sub y')
+//   x.subscribe(
+//     r => console.log("y r:", r),
+//     e => console.log("y e:", e))
+// }, 6000)
 
-setTimeout(() => {
-  console.log('new sub z')
-  x.subscribe(
-    r => console.log("z r:", r),
-    e => console.log("z e:", e))
-}, 7000)
+// setTimeout(() => {
+//   console.log('new sub z')
+//   x.subscribe(
+//     r => console.log("z r:", r),
+//     e => console.log("z e:", e))
+// }, 7000)
 
 export default {
   name: 'app',
